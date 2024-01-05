2ad1e0f33098f1e611f6e52a02b6f800521e81b6
Houssam Benlagha <houssambenlagha0@gmail.com>
2024-01-05 11:08:09 +0100

    Authentication added

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,16 +1,15 @@
+import { useNavigate } from "react-router-dom";
 import styles from "./User.module.css";
-
-const FAKE_USER = {
-  name: "Jack",
-  email: "jack@example.com",
-  password: "qwerty",
-  avatar: "https://i.pravatar.cc/100?u=zz",
-};
+import { useAuth } from "../contexts/FakeAuthContext";
 
 function User() {
-  const user = FAKE_USER;
+  const navigate = useNavigate();
+  const { user } = useAuth();
+  //const user = FAKE_USER;
 
-  function handleClick() {}
+  function handleClick() {
+    navigate("/");
+  }
 
   return (
     <div className={styles.user}>
